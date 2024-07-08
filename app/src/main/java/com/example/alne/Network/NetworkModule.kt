e3ddd9e546d269b6b431bfa3358644b4f48bb90e
yunsuk990 <yunsuk990@naver.com>
2024-07-08 23:51:16 +0900

    Add OkHttp Interceptor

--- app/src/main/java/com/example/alne/Network/NetworkModule.kt
+++ app/src/main/java/com/example/alne/Network/NetworkModule.kt
@@ -1,19 +1,31 @@
 package com.example.alne.Network
+import com.example.alne.GlobalApplication
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.ResponseBody
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.reflect.Type
+import java.util.concurrent.TimeUnit
 
 
 // http://ec2-54-175-121-98.compute-1.amazonaws.com:8080/
 const val BASE_URL = "http://10.0.2.2:3000"
 
 fun getRetrofit(): Retrofit {
+
+    val builder = OkHttpClient.Builder()
+        .addInterceptor(baseParameterInterceptor)
+        .build()
+
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
 //        .addConverterFactory(nullOnEmptyConverterFactory)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(builder)
         .build()
     return retrofit
 }
@@ -24,5 +36,21 @@ private val nullOnEmptyConverterFactory = object : Converter.Factory() {
         Converter<ResponseBody, Any?> {
         val nextResponseBodyConverter = retrofit.nextResponseBodyConverter<Any?>(converterFactory(), type, annotations)
         override fun convert(value: ResponseBody) = if (value.contentLength() == 0L) null else nextResponseBodyConverter.convert(value)
+    }
+}
+
+// 기본 파라미터 추가
+val baseParameterInterceptor: Interceptor = object: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        if(originalRequest.headers["Auth"] == "false"){
+            val finalRequest = chain.request().newBuilder()
+                .removeHeader("Auth")
+                .build()
+            return chain.proceed(finalRequest)
+        }
+        // Auth 파라미터 추가
+        val finalRequest = originalRequest.newBuilder().addHeader("Authorization", "Bearer ${GlobalApplication.prefManager.getUserToken().accessToken}").build()
+        return chain.proceed(finalRequest)
     }
 }
