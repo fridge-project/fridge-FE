e1a21a85a5573989d00939fddf48530b22178baa
yunsuk990 <yunsuk990@naver.com>
2024-07-09 23:39:20 +0900

    Update AuthApi.kt

--- app/src/main/java/com/example/alne/Network/AuthApi.kt
+++ app/src/main/java/com/example/alne/Network/AuthApi.kt
@@ -3,11 +3,11 @@ package com.example.alne.Network
 import com.example.alne.data.model.User
 import com.example.alne.data.model.ProfileRespond
 import com.example.alne.data.model.Token
-import com.example.alne.data.model.UserId
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -15,12 +15,15 @@ import retrofit2.http.Part
 interface AuthApi {
 
     @POST(API.SIGNUP)
+    @Headers("Auth: false")
     fun signUp(@Body user: User): Call<SignUpResponse>
 
     @POST(API.LOGIN)
+    @Headers("Auth: false")
     fun login(@Body user: User): Call<LoginResponse>
 
     @POST(API.LOGOUT)
+    @Headers("Auth: false")
     fun logout(@Body token: Token): Call<SignUpResponse>
 
     @Multipart
@@ -31,10 +34,10 @@ interface AuthApi {
     ): Call<AuthResponse>
 
 
-    @POST("/profile")
-    fun getUserProfile(
-        @Body userId: UserId
-    ):Call<ProfileRespond>
+//    @POST("/profile")
+//    fun getUserProfile(
+//        @Body userId: UserId
+//    ):Call<ProfileRespond>
 
 
 }
