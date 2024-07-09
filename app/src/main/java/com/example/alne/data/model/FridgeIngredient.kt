4840976c5e27d6315585a048c63bfb910afcdc68
yunsuk990 <yunsuk990@naver.com>
2024-07-09 23:37:42 +0900

    Update FridgeIngredient.kt

--- app/src/main/java/com/example/alne/data/model/FridgeIngredient.kt
+++ app/src/main/java/com/example/alne/data/model/FridgeIngredient.kt
@@ -6,6 +6,6 @@ data class FridgeIngredient(
     val storage: String,
     val exp: String,
     val date: String,
-    val imageURL: String? = "https://fridgeproject.s3.ap-northeast-2.amazonaws.com/default.png"
+    val imageURL: String? = ""
 )
 
