c08c4c89853c8ec1139051cb195cb8a3e522da07
yunsuk990 <yunsuk990@naver.com>
2024-07-18 23:37:54 +0900

    Update FridgeResponse.kt

--- app/src/main/java/com/example/alne/Network/FridgeResponse.kt
+++ app/src/main/java/com/example/alne/Network/FridgeResponse.kt
@@ -1,14 +1,7 @@
 package com.example.alne.Network
-
-import com.example.alne.data.model.Fridge
 import com.google.gson.annotations.SerializedName
 
 data class FridgePostResponse(
     @SerializedName(value = "status") val status: Int,
     @SerializedName(value = "data") val data: Int
-)
-
-data class FridgeGetResponse(
-    @SerializedName(value = "status") val status: Int,
-    @SerializedName(value = "data") val data: Array<Fridge>
 )
