29dc21b3cdd423eaa70e322cd3890a5cb0844397
yunsuk990 <yunsuk990@naver.com>
2024-07-18 23:37:46 +0900

    Update Recipe.kt

--- app/src/main/java/com/example/alne/data/model/Recipe.kt
+++ app/src/main/java/com/example/alne/data/model/Recipe.kt
@@ -1,43 +1,27 @@
 package com.example.alne.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Recipe(
-    val recipeCode: Int,
-    val name:String,
-    val introduce: String,
-    val category: String,
-    val classification: String,
-    val time: String,
-    val serving: String,
-    val difficulty: String,
-    val imageURL: String,
-    val calorie: Int,
-    val favoritedByUser: List<Int>,
-    val likedByUser: List<Int>
+    var _id: String,
+    var recipe_code: Int,
+    var recipe: String,
+    var introduce: String,
+    var category_code: Int,
+    var category: String,
+    var class_code: Int,
+    @SerializedName("class") var classification: String,
+    var time: String,
+    var calorie: String,
+    var serving: String,
+    var difficulty: String,
+    var imageURL: String,
+    var detailURL: String
 )
-
-data class RecipeProcessRespond(
-    val status: Int,
-    val data: RecipeProcess
-)
-
 data class RecipeProcess (
-    val recipe: Recipe,
-    val recipeProcess: List<Process>,
-    val comments: List<Comments>,
-    val gradeDto: Grade,
-    val favorite: Boolean,
-    val like: Boolean
-)
-
-data class Comments(
+    val recipe_code: Int,
+    val order_num: Int,
+    val detail: String,
     val imageURL: String,
-    val grade: Int,
-    val user: UserInfo,
-    val detail: String
-)
-
-data class Process(
-    val id: Int,
-    val orderNum: Int,
-    val detail: String
+    val tip: String,
 )
