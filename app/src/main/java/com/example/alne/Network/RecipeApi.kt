c49b575b1822df9286e24cef75d83f93d5cb198b
yunsuk990 <yunsuk990@naver.com>
2024-07-18 23:38:57 +0900

    레시피 API 수정

--- app/src/main/java/com/example/alne/Network/RecipeApi.kt
+++ app/src/main/java/com/example/alne/Network/RecipeApi.kt
@@ -6,54 +6,70 @@ import com.example.alne.data.model.FavoriteRespond
 import com.example.alne.data.model.FavoritesRespond
 import com.example.alne.data.model.Id
 import com.example.alne.data.model.LikeRespond
-import com.example.alne.data.model.RecipeProcessRespond
-import com.example.alne.data.model.UserId
+import com.example.alne.data.model.Recipe
 import com.example.alne.data.model.Status
-import com.example.alne.data.model.requestComment
+import com.example.alne.data.model.addComment
+import com.example.alne.room.model.recipe
+import com.google.gson.JsonArray
+import com.google.gson.JsonElement
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 
 interface RecipeApi {
 
-    @GET("/recipe")
-    fun getAllRecipe(): Call<RecipeResponse>
+    @GET(API.RECIPE_LIST)
+    @Headers("Auth: false")
+    fun getAllRecipe(): Call<ArrayList<Recipe>>
 
-    @POST("/addComment")
+    @POST(API.ADD_COMMENT)
     fun addUserComment(
-        @Body comment: Comment
-    ): Call<AuthResponse>
+        @Path(value = "recipe_id") recipe_id: String,
+        @Body comment: addComment
+    ): Call<JsonElement>
 
-    @POST("/recipe/{recipeCode}")
+    @GET(API.RECIPE_PROECSS)
+    @Headers("Auth: false")
     fun getRecipeProcess(
-        @Path("recipeCode") recipeCode: Int,
-        @Body userId: UserId
-    ): Call<RecipeProcessRespond>
+        @Path("recipe_code") recipeCode: Int,
+    ): Call<JsonArray>
 
-    @POST("/recipe/{recipeCode}/favorite")
-    fun addRecipeFavorite(
-        @Path("recipeCode") recipeCode: Int,
-        @Body userId: UserId
-    ): Call<FavoriteRespond>
+
+//
+//    @POST("/recipe/{recipeCode}/favorite")
+//    fun addRecipeFavorite(
+//        @Path("recipeCode") recipeCode: Int,
+//        @Body userId: UserId
+//    ): Call<FavoriteRespond>
 
     @POST("/favorites/delete")
     fun deleteRecipeFavorite(
         @Body delete: DeleteFavorite
     ): Call<Status>
 
-    @POST("/recipe/{recipeCode}/like")
-    fun likeRecipe(
-        @Path("recipeCode") recipeCode: Int,
-        @Body userid: UserId
-    ): Call<LikeRespond>
+//    @POST("/recipe/{recipeCode}/like")
+//    fun likeRecipe(
+//        @Path("recipeCode") recipeCode: Int,
+//        @Body userid: UserId
+//    ): Call<LikeRespond>
 
-    @POST("/delComment")
+    @POST(API.LIKE)
+    fun likeRecipe(
+        @Path("id") id: String
+    ): Call<String>
+
+    @GET(API.LIKE_LIST)
+    fun userLikeList(): Call<JsonArray>
+
+    @DELETE(API.DELETE_COMMENT)
     fun deleteUserComment(
-        @Body requestComment: requestComment
-    ): Call<AuthResponse>
+        @Path("id") id: String
+    ): Call<String>
 
 
     //사용자가 즐겨찾기 한 모든 레시피 정보 가져오기
