f4cc92e8e96f7b5842e87e872e75f2aad7164a6b
yunsuk990 <yunsuk990@naver.com>
2024-07-18 23:39:05 +0900

    Update recipe.kt

--- app/src/main/java/com/example/alne/room/model/recipe.kt
+++ app/src/main/java/com/example/alne/room/model/recipe.kt
@@ -2,17 +2,22 @@ package com.example.alne.room.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity
 data class recipe (
-    @PrimaryKey val recipe_code: Int,
-    val time: Int,
-    val difficulty: String,
-    val serving: String,
-    val name:String,
-    val imageurl: String,
-    val introduce: String,
-    val category: String,
-    val classification: String,
-    val calorie: Int,
+    @PrimaryKey var _id: String,
+    var recipe_code: Int,
+    var recipe: String,
+    var introduce: String,
+    var category_code: Int,
+    var category: String,
+    var class_code: Int,
+    var classification: String,
+    var time: String,
+    var calorie: String,
+    var serving: String,
+    var difficulty: String,
+    var imageURL: String,
+    var detailURL: String
 )
