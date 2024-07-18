f56262053aa87943181857a6d1a7ee1c49ff52ec
yunsuk990 <yunsuk990@naver.com>
2024-07-18 23:41:09 +0900

    Update RecipeDetailActivity.kt

--- app/src/main/java/com/example/alne/presentation/view/Recipe/RecipeDetailActivity.kt
+++ app/src/main/java/com/example/alne/presentation/view/Recipe/RecipeDetailActivity.kt
@@ -7,11 +7,10 @@ import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
-import com.example.alne.GlobalApplication
 import com.example.alne.R
 import com.example.alne.databinding.ActivityRecipeDetailBinding
-import com.example.alne.data.model.UserId
 import com.example.alne.room.model.recipe
+import com.example.alne.utils.RESPONSE_STATUS
 import com.example.alne.viewmodel.RecipeDetailViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 import com.google.gson.Gson
@@ -28,9 +27,10 @@ class RecipeDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.recipeDetailTb.bringToFront()
         globalrecipe = Gson().fromJson(intent.getStringExtra("recipe"), recipe::class.java)
+
         Log.d("RecipeDetailActivity_recipe", globalrecipe.toString())
         viewModel = ViewModelProvider(this).get(RecipeDetailViewModel::class.java)
-
+        viewModel.getRecipeProcess(globalrecipe!!.recipe_code)
 
 
     }
@@ -38,31 +38,16 @@ class RecipeDetailActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         init(globalrecipe!!)
-        Log.d("RecipeDetailActivity", "onResume")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("RecipeDetailActivity", "onResume")
-
-
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("RecipeDetailActivity", "onPause")
-
     }
 
     private fun init(recipe: recipe){
-//        viewModel.getRecipeProcess(recipe.recipe_code, UserId(GlobalApplication.prefManager.getUserToken()?.userId!!, null))
-        binding.recipeDetailTitleTv.text = recipe.name
+        binding.recipeDetailTitleTv.text = recipe.recipe
         binding.recipeDetailChefTv.text = recipe.difficulty
         binding.recipeDetailIntroduceTv.text = recipe.introduce
         binding.recipeDetailCategoryTv1.text = "#" + recipe.classification
         binding.recipeDetailTimeTv.text = "약 " + recipe.time + "분"
         binding.recipeDetailKcalTv.text = recipe.calorie.toString() + "kcal"
-        Glide.with(this@RecipeDetailActivity).load(recipe.imageurl).into(binding.recipeDetailFoodIv)
+        Glide.with(this@RecipeDetailActivity).load(recipe.imageURL).into(binding.recipeDetailFoodIv)
 
         val recipeAdapter = RecipeDetailVPAdapter(this@RecipeDetailActivity, recipe)
         binding.recipeDetailVp.adapter = recipeAdapter
@@ -87,12 +72,26 @@ class RecipeDetailActivity : AppCompatActivity() {
         })
 
         binding.like.setOnClickListener {
-            viewModel.userLikeRecipe(recipe.recipe_code)
-        }
+            viewModel.likeRecipe(recipe._id, completion = { responseStatus ->
+                when(responseStatus){
+                    RESPONSE_STATUS.OKAY -> {
+                        Log.d("like.setOnClickListener", "OKAY")
+                    }
 
-        binding.bookmark.setOnClickListener {
-            viewModel.addRecipeFavorite(recipe.recipe_code)
+                    RESPONSE_STATUS.FAIL -> {
+                        Log.d("like.setOnClickListener", "FAIL")
+                    }
+
+                    RESPONSE_STATUS.NETWORK_ERROR -> {
+                        Log.d("like.setOnClickListener", "NETWORK_ERROR")
+                    }
+                }
+            })
         }
+//
+//        binding.bookmark.setOnClickListener {
+//            viewModel.addRecipeFavorite(recipe.recipe_code)
+//        }
         setOnClickListener()
     }
 
