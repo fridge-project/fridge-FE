f33f8dae93f5c11bcbcf9e1bba96397fa0e25d1a
yunsuk990 <yunsuk990@naver.com>
2024-07-23 12:39:23 +0900

    Update IngredientChoiceViewModel.kt

--- app/src/main/java/com/example/alne/presentation/viewmodel/IngredientChoiceViewModel.kt
+++ app/src/main/java/com/example/alne/presentation/viewmodel/IngredientChoiceViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.alne.viewmodelprivate
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.alne.room.model.food
