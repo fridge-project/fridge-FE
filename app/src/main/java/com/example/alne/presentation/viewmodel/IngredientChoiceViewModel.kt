a82b72b1c4f4e9b2e00b447b181f1d7faae1d671
yunsuk990 <yunsuk990@naver.com>
2024-07-27 18:28:36 +0900

    Update IngredientChoiceViewModel.kt

--- app/src/main/java/com/example/alne/presentation/viewmodel/IngredientChoiceViewModel.kt
+++ app/src/main/java/com/example/alne/presentation/viewmodel/IngredientChoiceViewModel.kt
@@ -1,12 +1,11 @@
 package com.example.alne.viewmodelprivate
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.example.alne.room.model.food
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.alne.repository.fridgeRepository
+import com.example.alne.data.model.RoomIngredient
+import com.example.alne.data.model.repository.fridgeRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -15,8 +14,8 @@ class IngredientChoiceViewModel: ViewModel() {
     private val repository = fridgeRepository()
 
     //재료 초기화
-    private val _getFridgeLiveData = MutableLiveData<ArrayList<food>>()
-    val getFridgeLiveData: LiveData<ArrayList<food>> = _getFridgeLiveData
+    private val _getFridgeLiveData = MutableLiveData<ArrayList<RoomIngredient>>()
+    val getFridgeLiveData: LiveData<ArrayList<RoomIngredient>> = _getFridgeLiveData
 
     init {
         viewModelScope.launch(Dispatchers.IO){
