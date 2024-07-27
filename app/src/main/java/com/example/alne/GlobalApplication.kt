de882a586428f1e90e3ca3590cbe7104997aa4f6
yunsuk990 <yunsuk990@naver.com>
2024-07-27 18:27:27 +0900

    Update GlobalApplication.kt

--- app/src/main/java/com/example/alne/GlobalApplication.kt
+++ app/src/main/java/com/example/alne/GlobalApplication.kt
@@ -2,17 +2,18 @@ package com.example.alne
 
 import android.app.Application
 import android.util.Log
-import com.example.alne.Network.RecipeApi
+import com.example.alne.Network.RecipeService
 import com.example.alne.Network.getRetrofit
+import com.example.alne.data.model.FridgeIngredient
 import com.example.alne.data.model.Recipe
+import com.example.alne.data.model.RoomIngredient
 import com.example.alne.room.AppDatabase
 import com.example.alne.room.model.recipe
-import com.example.alne.utils.SharedPrefManager
+import com.example.alne.domain.utils.SharedPrefManager
+import com.google.gson.JsonArray
 import com.kakao.sdk.common.KakaoSdk
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
@@ -36,7 +37,7 @@ class GlobalApplication():Application() {
         retrofit = getRetrofit()
 
         if(!prefManager.getfirstApp()){
-            retrofit.create(RecipeApi::class.java).getAllRecipe().enqueue(object : Callback<ArrayList<Recipe>> {
+            retrofit.create(RecipeService::class.java).getAllRecipe().enqueue(object : Callback<ArrayList<Recipe>> {
                 override fun onResponse(
                     p0: Call<ArrayList<Recipe>>,
                     p1: Response<ArrayList<Recipe>>,
@@ -78,6 +79,34 @@ class GlobalApplication():Application() {
                 override fun onFailure(p0: Call<ArrayList<Recipe>>, p1: Throwable) {
 
                 }
+            })
+
+
+            retrofit.create(RecipeService::class.java).ingredientsList().enqueue(object: Callback<JsonArray>{
+                override fun onResponse(p0: Call<JsonArray>, p1: Response<JsonArray>) {
+                    val res = p1.body()
+                    when(p1.code()){
+                        200 ->{
+                            CoroutineScope(Dispatchers.IO).launch {
+                                for(json in res!!){
+                                    var jsonObject = json.asJsonObject
+                                    var _id = jsonObject.get("_id").asString
+                                    var name = jsonObject.get("name").asString
+                                    appDatabase.ingredientDao().insertIngredient(RoomIngredient(_id,name))
+                                }
+                            }
+                        }
+
+                        500 -> {
+
+                        }
+                    }
+                }
+
+                override fun onFailure(p0: Call<JsonArray>, p1: Throwable) {
+
+                }
+
             })
             prefManager.savefirstApp()
         }
