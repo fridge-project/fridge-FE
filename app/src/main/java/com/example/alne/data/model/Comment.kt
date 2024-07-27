70c6c2f8f7c602af0da83cfa49523052fa166480
yunsuk990 <yunsuk990@naver.com>
2024-07-27 18:26:45 +0900

    Update Comment.kt

--- app/src/main/java/com/example/alne/data/model/Comment.kt
+++ app/src/main/java/com/example/alne/data/model/Comment.kt
@@ -3,11 +3,11 @@ package com.example.alne.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Comment(
+    var _id: String,
     var user_id: String,
     var recipe_id: String,
     var detail: String,
     var grade: Int,
-    var _id: String,
     var __v: Int
 )
 
