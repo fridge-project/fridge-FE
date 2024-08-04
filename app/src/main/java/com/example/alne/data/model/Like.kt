d97ff60d85baeee9147d8b64d02685fdab2eaed4
yunsuk990 <yunsuk990@naver.com>
2024-08-04 23:47:13 +0900

    Update Like.kt

--- app/src/main/java/com/example/alne/data/model/Like.kt
+++ app/src/main/java/com/example/alne/data/model/Like.kt
@@ -1,15 +1,9 @@
 package com.example.alne.data.model
 
-data class LikeRespond(
-    val status: Int,
-    val data: Like
-)
-
 data class Like(
-    val id: Int,
-    val user: UserInfo,
-    val recipe: Recipe,
-    val like: Boolean
+    val _id: String,
+    val user_id: String,
+    val recipe_id: String
 )
 
 
