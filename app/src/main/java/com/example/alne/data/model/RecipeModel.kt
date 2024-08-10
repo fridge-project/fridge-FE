c52f28d92411c8b24d1c2b5193455f4cbdb96760
yunsuk990 <yunsuk990@naver.com>
2024-08-10 13:19:16 +0900

    Create RecipeModel.kt

---
+++ app/src/main/java/com/example/alne/data/model/RecipeModel.kt
@@ -0,0 +1,27 @@
+package com.example.alne.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class RecipeModel(
+    var _id: String,
+    var recipe_code: Int,
+    var recipe: String,
+    var introduce: String,
+    var category_code: Int,
+    var category: String,
+    var class_code: Int,
+    @SerializedName("class") var classification: String,
+    var time: String,
+    var calorie: String,
+    var serving: String,
+    var difficulty: String,
+    var imageURL: String,
+    var detailURL: String
+)
+data class RecipeProcess (
+    val recipe_code: Int,
+    val order_num: Int,
+    val detail: String,
+    val imageURL: String,
+    val tip: String,
+)
